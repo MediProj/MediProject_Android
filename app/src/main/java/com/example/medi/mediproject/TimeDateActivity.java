9a3e3642af1f25601fad975aa220360b9d59d80f
Hummingbird-12 <goodboyih96@gmail.com>
2018-08-12 21:01:16 +0900

    implement DatePicker and TimePicker spinners

--- app/src/main/java/com/example/medi/mediproject/TimeDateActivity.java
+++ app/src/main/java/com/example/medi/mediproject/TimeDateActivity.java
@@ -1,13 +1,11 @@
 package com.example.medi.mediproject;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -15,32 +13,35 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.util.Calendar;
-
-import static java.util.Calendar.DATE;
+import java.sql.Time;
 
 public class TimeDateActivity extends BaseActivity {
-    Button bt_prev, bt_next, bt_time,bt_date;
-    TextView title,tv;
-    final int DIALOG_DATE = 1;
-    final int DIALOG_TIME = 2;
+    Button bt_prev, bt_next, bt_pick; //, bt_time,bt_date;
+    TextView tv, res;
+    DatePicker pickDate;
+    TimePicker pickTime;
+
     String name =null;
     String pid,str_pk;
     int page_id=0;
-    int year, month, day, hour, minute,Shour, Sminute;
-    boolean date_flag=false;
-    boolean time_flag=false;
-    boolean today_flag = false;
+    int year, month, day, hour, minute;
+    boolean dateSet = false;
 
     public void onCreate(Bundle SavedInstanceState){
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_time_date);
 
         tv=findViewById(R.id.tv);
-        bt_next=findViewById(R.id.Bnt_next);
-        bt_prev=findViewById(R.id.Bnt_prev);
-        bt_date=findViewById(R.id.bt_date);
-        bt_time=findViewById(R.id.bt_time);
+
+        bt_next = findViewById(R.id.Bnt_next);
+        bt_prev = findViewById(R.id.Bnt_prev);
+
+        bt_pick = findViewById(R.id.btnPicker);
+
+        pickDate = findViewById(R.id.picD);
+        pickTime = findViewById(R.id.picT);
+
+        res = findViewById(R.id.pickResult);
 
         //title 표시
         final Intent intent = getIntent();
@@ -51,27 +52,21 @@ public class TimeDateActivity extends BaseActivity {
 
         tv.setText(name +"님 날짜와 시간을 선택하고\n다음 버튼을 눌러주세요");
 
-        //Date
-        bt_date.setOnClickListener(new View.OnClickListener() {
+        res.setText(R.string.promptPick);
+
+        // Select Time button
+        bt_pick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDialog(DIALOG_DATE);
+                updateDateTime();
             }
         });
 
-        //Time
-        bt_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDialog(DIALOG_TIME);
-            }
-        });
-
-        //prev(Menu page)]
+        //prev(Menu page)
         bt_prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(TimeDateActivity.this,PreActivity.class);
+                Intent intent2 = new Intent(TimeDateActivity.this, TimeDateActivity.class);
                 intent2.putExtra("val",page_id);
                 intent2.putExtra("pid",pid);
                 startActivity(intent2);
@@ -83,11 +78,23 @@ public class TimeDateActivity extends BaseActivity {
         bt_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!date_flag)
-                    Toast.makeText(getApplicationContext(), "날짜를 선택해 주세요", Toast.LENGTH_SHORT).show();
+                if (!dateSet) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(TimeDateActivity.this);
+                    LayoutInflater inf = TimeDateActivity.this.getLayoutInflater();
 
-                else if (!time_flag)
-                    Toast.makeText(getApplicationContext(), "시간을 선택해 주세요", Toast.LENGTH_SHORT).show();
+                    alertBuilder.setView(inf.inflate(R.layout.date_time_pick_alert, null));
+                    alertBuilder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+
+                    AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                    alert.getWindow().setLayout(700, 250);
+                }
 
                 //다음 페이지로 이동
                 else {
@@ -115,70 +122,16 @@ public class TimeDateActivity extends BaseActivity {
 
     }
 
-    public Dialog onCreateDialog(int id){
-        today_flag=false;
-        Calendar cal = Calendar.getInstance();
-        year= cal.get(Calendar.YEAR);
-        month= cal.get(Calendar.MONTH);
-        day = cal.get(DATE);
-        hour = cal.get(Calendar.HOUR_OF_DAY);
-        minute=cal.get(Calendar.MINUTE);
+    public void updateDateTime() {
+        year = pickDate.getYear();
+        month = pickDate.getMonth() + 1; // Jan is 0
+        day = pickDate.getDayOfMonth();
 
-        switch(id){
-            case DIALOG_DATE :
-                DatePickerDialog datePickerDialog = new DatePickerDialog(TimeDateActivity.this, AlertDialog.THEME_HOLO_LIGHT,
-                        new DatePickerDialog.OnDateSetListener(){
-                    public void onDateSet(DatePicker view, int syear, int smonth, int sday){
-                        Toast.makeText(getApplicationContext(),syear+"년 "+(smonth+1)+"월 "+sday +"일을 선택했습니다",Toast.LENGTH_SHORT).show();
-                        date_flag=true;
+        hour = pickTime.getCurrentHour();
+        minute = pickTime.getCurrentMinute();
 
-                        if(syear==year && smonth==month && sday==day)
-                            today_flag =true;
-                    }
-                        },year,month,day);
+        res.setText("선택하신 날짜와 시간: " + String.format("%d년 %d월 %d일 %d시 %d분", year, month, day, hour, minute));
 
-                // DatePicker set up
-                datePickerDialog.getDatePicker().updateDate(year, month, day);
-                datePickerDialog.getDatePicker().setCalendarViewShown(true);
-                datePickerDialog.getDatePicker().setSpinnersShown(false);
-                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-                datePickerDialog.getDatePicker().setScaleX(1.6f);
-                datePickerDialog.getDatePicker().setScaleY(1.6f);
-                datePickerDialog.getDatePicker().setPadding(100, 180, 100, 100);
-                datePickerDialog.getDatePicker().setForegroundGravity(Gravity.CENTER);
-                datePickerDialog.getDatePicker().setMinimumWidth(1000);
-                datePickerDialog.getDatePicker().setMinimumHeight(800);
-
-                // CalendarView set up
-                datePickerDialog.getDatePicker().getCalendarView().setShowWeekNumber(false);
-                datePickerDialog.getDatePicker().getCalendarView().setDate(Calendar.getInstance().getTimeInMillis(), true, true);
-                datePickerDialog.getDatePicker().getCalendarView().setUnfocusedMonthDateColor(getResources().getColor(R.color.Gray2));
-
-                datePickerDialog.show();
-
-                // Dialog Button set up
-                datePickerDialog.getButton(Dialog.BUTTON_POSITIVE).setTextSize(40);
-                datePickerDialog.getButton(Dialog.BUTTON_POSITIVE).setTypeface(null, Typeface.BOLD);
-                datePickerDialog.getButton(Dialog.BUTTON_POSITIVE).setText("확인");
-                datePickerDialog.getButton(Dialog.BUTTON_NEGATIVE).setTextSize(40);
-                datePickerDialog.getButton(Dialog.BUTTON_NEGATIVE).setText("취소");
-
-                return datePickerDialog;
-
-            case DIALOG_TIME :
-                final TimePickerDialog timePickerDialog = new TimePickerDialog(TimeDateActivity.this,
-                        AlertDialog.THEME_HOLO_LIGHT,  new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int shour, int sminute) {
-                        Toast.makeText(getApplicationContext(),shour+"시 "+ sminute +"분을 선택했습니다",Toast.LENGTH_SHORT).show();
-                        time_flag=true;
-                    }
-                }, hour,minute,false);
-
-                return timePickerDialog;
-
-        }
-        return super.onCreateDialog(id);
+        dateSet = true;
     }
-
 }
