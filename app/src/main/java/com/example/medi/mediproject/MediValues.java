3783dc2857a30bd491756c182d111e7b4162fa94
ina-uzu <ina.uzu.song@gmail.com>
2018-08-13 15:20:03 +0900

    test commit!!!!

--- app/src/main/java/com/example/medi/mediproject/MediValues.java
+++ app/src/main/java/com/example/medi/mediproject/MediValues.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class MediValues {
     public static final String USERNAME = "service";
-    public static final String PASSWORD = "service@service";
+    public static final String PASSWORD = "medi@service";
     public static String ACCESS_TOKEN = "";
 
     public static Map<String, Map<String, String>> patientData;
